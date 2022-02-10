e12f63927a93a7d78b533a047e7ab9eba2079a18
nam.le <nam.le@opswat.com>
2022-02-10 08:58:00 +0700

    Initialization for git bash aliases and git aliases

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+### C:\Program Files\Git\etc\profile.d
+
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+# --show-control-chars: help showing Korean or accented characters
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+alias gst='git status'
+alias gl='git log'
+alias gp='git push'
+alias gpr='git pull --rebase'
+alias gcm='git commit'
+alias gsw='git switch'
+alias gf='git fetch'
+alias ga='git add'
+alias gau='git add -u'
---
+++ git-alias.sh
@@ -0,0 +1,2 @@
+git conifg --global alias.sw switch
+git config --global alias.ck checkout
